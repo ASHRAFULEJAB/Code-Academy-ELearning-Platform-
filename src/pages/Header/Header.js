8236c624df116ff645fc862de6232ec6e4947de2
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-10-27 21:48:01 +0600

    enviroment added

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -11,7 +11,7 @@ import HeaderSwitch from './HeaderSwitch'
 
 const Header = () => {
   const { user, logOut } = useContext(CourseContext)
-  const  {darkMode}=useContext(DarkModeContext)
+  const { darkMode } = useContext(DarkModeContext)
 
   const handleLogout = () => {
     logOut()
@@ -93,36 +93,17 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              
-                
-              <div className={darkMode ? 'Header header-dark' : 'Header header-light'}>
-                <HeaderSwitch/>
-              </div>
-         
-              {/* <label
-                for='Toggle1'
-                className='inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100'
-              >
-                {
-                  <span>Left</span>:<span>Right</span>
+              <div
+                className={
+                  darkMode ? 'Header header-dark' : 'Header header-light'
                 }
-               
-                <span className='relative'>
-                  <input id='Toggle1' type='checkbox' className='hidden peer' />
-                  <div
-                    className='w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400'
-                    bis_skin_checked='1'
-                  ></div>
-                  <div
-                    className='absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800'
-                    bis_skin_checked='1'
-                  ></div>
-                </span>
-                
-              </label> */}
+              >
+                <HeaderSwitch />
+              </div>
             </li>
             {user?.photoURL ? (
-              <div
+              <Link to='/profile'>
+                <div
                 data-tip={user.displayName}
                 data-for='toolTip1'
                 data-place='top'
@@ -134,6 +115,7 @@ const Header = () => {
                 />
                 <ReactTooltip id='toolTip1' />
               </div>
+              </Link>
             ) : (
               <li>
                 <Link
